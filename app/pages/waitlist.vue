22502b02217accb5e9b3cd47ffce38e0b965dc87
Shirsendu Bairagi <shirsendu2001@gmail.com>
2026-09-07 20:34:31 +0530

    fix: update target date for waitlist and improve template formatting

--- app/pages/waitlist.vue
+++ app/pages/waitlist.vue
@@ -56,7 +56,7 @@ const onSubmit = async () => {
 }
 
 // const waitingQueueCount = ref(0)
-const targetDate = new Date('2026-09-01T00:00:00')
+const targetDate = new Date('2026-09-15T00:00:00')
 const now = useNow()
 
 const timeRemaining = computed(() => Math.max(0, targetDate.getTime() - now.value.getTime()))
